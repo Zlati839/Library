947c2891ddceed6d1974d5f2eb157b807f8c9f56
Zlati839 <zlatipenev7@gmail.com>
2026-02-17 14:27:37 +0200

    Correct write statement for books display
    Fix typo in the write statement for displaying books.

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.title("Book library📚")
 add_book = st.text_input("Add a book")
 if st.button("ADD"):
    st.session_state['books'].append(add_book) 
-   st.write(books)
+   st.write('books')
 #============APP=============
 st.write("Enter a titl you want to search for")
 input = st.text_input("Book ?")
