dd2de5a2ac7581dcbb53fcc452ccda7e7f789987
Zlati839 <zlatipenev7@gmail.com>
2026-02-17 13:59:15 +0200

    Create App.py

---
+++ App.py
@@ -0,0 +1,21 @@
+import streamlit as st
+#===========Books===============
+books = ["The Seven Husbands of Evelyn Hugo"
+        "To Kill a Mockingbird"
+        "The Hunger Games"
+        "The Alchemist"
+        "The Fault in Our Stars"
+        "The Midnight Library"
+        "The Book Thief"
+        ]
+#============APP=============
+st.title("Book library📚")
+st.write("Enter a titl you want to search for")
+input = st.text_input("Book ?")
+if st.button("Check"):
+  if input.strip == "":
+     st.warning("Input a book"):
+  elif input in books:
+    st.success("The book is in the database",input)
+  else:
+    st.warning("The book " ,input,"is not in the data base ")
